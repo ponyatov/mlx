09345bb8d4060bd67d9a44f0bc48235217037ef6
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-02 14:57:52 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/mlx.ml
+++ lib/mlx.ml
@@ -1,7 +1,9 @@
-
-let dirs =
-  Sys.mkdir ".vscode" 0o750
+let dirs = Sys.mkdir ".vscode" 0o750
 
 let settings =
-  let f = open_out ".vscode/settings.json"  in
+  let f = open_out ".vscode/settings.json" in
+  close_out f
+
+let extensions =
+  let f = open_out ".vscode/extensions.json" in
   close_out f
